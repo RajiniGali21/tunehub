4e0fe64d38ab09c5730433bbbd34f9a5a9b86778
Rajini-56 <105849708+Rajini-56@users.noreply.github.com>
2024-01-31 15:54:19 +0530

    1st-version of tune hub.

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,23 +1,35 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.LoginData;
+import com.example.demo.entity.Song;
 import com.example.demo.entity.Users;
+import com.example.demo.services.SongService;
 import com.example.demo.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
-
+@CrossOrigin("*")
+@RestController
 @Controller
 public class UsersController 
 {
 @Autowired
 UsersService service;
+@Autowired
+SongService songService;
 @PostMapping("/register")
 //public String addUsers(@RequestParam("username")String username,
 //		@RequestParam("email")String email,
@@ -42,8 +54,11 @@ return "home";
 }
 
 @PostMapping("/validate")
-public String postMethodName(@RequestParam("email")String email, @RequestParam("password") String password, HttpSession session)
+public String validate(@RequestBody LoginData data, HttpSession session,Model model)
 {
+	System.out.println("call received");
+	String email = data.getEmail();
+	String password = data.getPassword();
 	if(service.validateUser(email,password)==true)
 	{
 		String role=service.getRole(email);
@@ -54,6 +69,12 @@ public String postMethodName(@RequestParam("email")String email, @RequestParam("
 		}
 		else
 		{
+			Users user = service.getUser(email);
+			boolean userStatus = user.isPremium();
+			List<Song> songsList = songService.fetchAllSongs();
+			model.addAttribute("songs",songsList);
+			model.addAttribute("isPremium",userStatus);
+			
 			return "customerHome";
 		}
 	}
